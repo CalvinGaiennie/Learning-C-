49883283984adaccf2e624a6657155b403831f9b
CalvinGaiennie <calvingaiennie@gmail.com>
2024-11-25 13:46:21 -0600

    Did the first 4 lessons of a youtube course

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Array CRUD
+
+// Create
+string[] favoriteRats = ["fancy rat", "brown rat", "radioactive rat", "wolf rat"];
+
+// Read
+
+// foreach(var rat in favoriteRats)
+// {
+//     Console.WriteLine(rat);
+// };
+
+// Update
+// favoriteRats[0] = "Fancy Rat";
+// LINQ - 
+
+var newFavoriteRats = favoriteRats.Where((e) => e.StartsWith("b"));
+
+foreach(var rat in newFavoriteRats)
+{
+    Console.WriteLine(rat);
+};
+
+// Delete
+// gimme
